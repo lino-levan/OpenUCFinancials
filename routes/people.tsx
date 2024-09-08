27bef4af9b9299c5fad6174425d5a1b37a8644b8
Lino Le Van <11367844+lino-levan@users.noreply.github.com>
2024-09-07 21:07:33 -0700

    feat: expand the size of people menu

--- routes/people.tsx
+++ routes/people.tsx
@@ -4,7 +4,7 @@ import { EmployeeTable } from "islands/EmployeeTable.tsx";
 export default async function Home() {
   return (
     <div class="w-screen h-screen items-center overflow-y-auto p-4">
-      <div class="max-w-screen-lg mx-auto space-y-4">
+      <div class="max-w-screen-xl mx-auto space-y-4">
         <a
           href="/"
           class="flex items-center gap-1 text-gray-700 hover:text-black hover:underline"
